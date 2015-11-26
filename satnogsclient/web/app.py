414f0758a004f506c15a1af10ec84242f89a8390
Corey Shields <cshields@gmail.com>
2015-11-25 19:35:12 -0500

    Add an observations view to client dashboard
    This commit adds a page and template that show the currently
    queued observations in the local job queue for that ground station.

--- satnogsclient/web/app.py
+++ satnogsclient/web/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 
 from satnogsclient import settings as client_settings
-
+from satnogsclient.scheduler import scheduler
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -22,3 +23,20 @@ def index():
     }
 
     return render_template('index.j2', **ctx)
+
+
+@app.route("/observations")
+def observations():
+    """View list of queued observation jobs."""
+    observations = []
+    for obs in scheduler.get_jobs():
+        if obs.name == "spawn_observer":
+            obs.kwargs['obj']['start'] = '{:%Y-%m-%d %H:%M}'.format(
+                datetime.strptime(obs.kwargs['obj']['start'], '%Y-%m-%dT%H:%M:%SZ'))
+            obs.kwargs['obj']['end'] = '{:%Y-%m-%d %H:%M}'.format(
+                datetime.strptime(obs.kwargs['obj']['end'], '%Y-%m-%dT%H:%M:%SZ'))
+            obs.kwargs['obj']['frequency'] = '{:.3f}'.format(
+                obs.kwargs['obj']['frequency'] / 1000000.0)
+            observations.append(obs)
+
+    return render_template('observations.j2', observations=observations)
